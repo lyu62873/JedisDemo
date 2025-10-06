93c2a8450fb88d2b4517e52886dd423f34dc545e
Lyu <lyuj@uchicago.edu>
2025-10-05 23:51:45 -0500

    test Hash set/get

--- src/test/java/com/leyu/jedisdemo/JedisDemoApplicationTests.java
+++ src/test/java/com/leyu/jedisdemo/JedisDemoApplicationTests.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import redis.clients.jedis.Jedis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
 class JedisDemoApplicationTests {
 
@@ -23,6 +26,20 @@ class JedisDemoApplicationTests {
         System.out.println("result: " + result);
         String getResult = jedis.get("name");
         System.out.println("getResult: " + getResult);
+    }
+
+    @Test
+    void testHash() {
+        Map<String, String> pokeMap = new HashMap<>();
+        pokeMap.put("1000", "赛富豪");
+        pokeMap.put("386", "代欧奇希斯");
+
+        String result = jedis.hmset("pokemon", pokeMap);
+        System.out.println("result: " + result);
+        String getResult = jedis.hget("pokemon", "1000");
+        System.out.println("1000 getResult: " + getResult);
+        Map<String, String> pokemon = jedis.hgetAll("pokemon");
+        System.out.println("pokemon: " + pokemon);
     }
 
     @AfterEach
