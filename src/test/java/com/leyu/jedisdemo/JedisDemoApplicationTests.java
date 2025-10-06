b3b371c693e8307ffb731b220db95b1d9bee0fda
Leyu <lyu62873@gmail.com>
2025-10-05 20:01:39 -0500

    initial commit

---
+++ src/test/java/com/leyu/jedisdemo/JedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyu.jedisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
