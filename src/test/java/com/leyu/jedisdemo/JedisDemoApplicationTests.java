f3680760613ab2b15797b4c4b8088916e0fb8a6f
Lyu <lyuj@uchicago.edu>
2025-10-05 23:54:34 -0500

    back

--- src/test/java/com/leyu/jedisdemo/JedisDemoApplicationTests.java
+++ src/test/java/com/leyu/jedisdemo/JedisDemoApplicationTests.java
@@ -27,21 +27,7 @@ class JedisDemoApplicationTests {
         String getResult = jedis.get("name");
         System.out.println("getResult: " + getResult);
     }
-
-    @Test
-    void testHash() {
-        Map<String, String> pokeMap = new HashMap<>();
-        pokeMap.put("1000", "赛富豪");
-        pokeMap.put("386", "代欧奇希斯");
-
-        String result = jedis.hmset("pokemon", pokeMap);
-        System.out.println("result: " + result);
-        String getResult = jedis.hget("pokemon", "1000");
-        System.out.println("1000 getResult: " + getResult);
-        Map<String, String> pokemon = jedis.hgetAll("pokemon");
-        System.out.println("pokemon: " + pokemon);
-    }
-
+    
     @AfterEach
     void tearDown() {
         if (jedis != null) {
