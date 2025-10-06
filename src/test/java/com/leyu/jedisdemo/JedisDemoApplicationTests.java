9f56c3a1dc45c7df8fddcac7db70a4007c8784d7
Leyu <lyu62873@gmail.com>
2025-10-05 23:44:35 -0500

    test String set/get

--- src/test/java/com/leyu/jedisdemo/JedisDemoApplicationTests.java
+++ src/test/java/com/leyu/jedisdemo/JedisDemoApplicationTests.java
@@ -1,13 +1,35 @@
 package com.leyu.jedisdemo;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
 
 @SpringBootTest
 class JedisDemoApplicationTests {
 
+    private Jedis jedis;
+
+    @BeforeEach
+    void setUp() {
+        jedis = new Jedis("43.166.246.21", 6379);
+        jedis.select(0);
+    }
+
     @Test
-    void contextLoads() {
+    void testString() {
+        String result = jedis.set("name", "张三"); //这里的result指set的结果是否成功
+        System.out.println("result: " + result);
+        String getResult = jedis.get("name");
+        System.out.println("getResult: " + getResult);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (jedis != null) {
+            jedis.close();
+        }
     }
 
 }
